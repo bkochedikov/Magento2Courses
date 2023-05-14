7e6c300e74fdbdfa0a5e07aa8374d917ae7c3032
bkochedikov <bkochedikov27946@gmail.com>
2023-05-14 21:11:59 +0300

    Update code before ex 4

--- app/code/Tsg/Blog/Controller/Post/Detail.php
+++ app/code/Tsg/Blog/Controller/Post/Detail.php
@@ -3,11 +3,20 @@
 namespace Tsg\Blog\Controller\Post;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
 
 class Detail implements HttpGetActionInterface
 {
-    public function execute()
+    private $pageFactory;
+    public function __construct(
+        PageFactory $pageFactory
+    ){
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute(): Page
     {
-        die("Blog post detail");
+        return $this->pageFactory->create();
     }
 }
